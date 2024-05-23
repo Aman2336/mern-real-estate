f2b90dc75409eee9fe114068fe377b71326fe15d
Aman Dhingra <amandhingra2336@gmail.com>
2024-05-23 19:20:27 +0530

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -30,7 +30,7 @@ export default function Header() {
     <header className="bg-slate-500 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
-          <h1 className="font-bold text-sm text-xl">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
             <span className="text-slate-600">Real</span>
             <span className="text-slate-700">Estate</span>
           </h1>
@@ -42,7 +42,7 @@ export default function Header() {
           <input
             type="text"
             placeholder="Search..."
-            className="bg-transparent focus:outline-none w-64"
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
             value={searchterm}
             onChange={(e) => {
               setsearchterm(e.target.value);
